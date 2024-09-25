d61a13fb23c7d164c9abb20d8fa7ec250cbfe46a
Maffei <bruno.maffei@lecom.com.br>
2024-09-25 17:57:52 -0300

    arrumando id messages

--- src/app/messages/[id].js
+++ src/app/messages/[id].js
@@ -9,9 +9,23 @@ export default function MessagePage() {
 
   useEffect(() => {
     if (id) {
-      fetch(`/api/messages?id=${id}`)
-        .then((res) => res.json())
-        .then((data) => setMessageData(data));
+      const fetchMessage = async () => {
+        try {
+          const res = await fetch(
+            `${window.location.origin}/api/messages?id=${id}`
+          );
+          if (!res.ok) {
+            throw new Error("Erro ao buscar a mensagem");
+          }
+          const data = await res.json();
+          setMessageData(data);
+        } catch (error) {
+          console.error("Erro:", error);
+          setMessageData({ error: error.message });
+        }
+      };
+
+      fetchMessage();
     }
   }, [id]);
 
@@ -19,15 +33,21 @@ export default function MessagePage() {
     return <p>Carregando...</p>;
   }
 
+  if (messageData.error) {
+    return <p>{messageData.error}</p>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full text-center">
         <h1 className="text-2xl font-bold mb-4">Mensagem Confidencial</h1>
         <p className="text-gray-800 mb-4">{messageData.message}</p>
-        {messageData.imagePath && (
+        {messageData.imageUrl && (
           <Image
-            src={messageData.imagePath}
+            src={messageData.imageUrl}
             alt="Imagem Confidencial"
+            width={500}
+            height={300}
             className="max-w-full h-auto rounded-lg shadow-lg"
           />
         )}
